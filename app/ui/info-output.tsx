52c8a7cb68fa018862c5d26920fabea8f9633124
Fraser Kent <fwpkent@gmail.com>
2026-04-26 19:42:53 -0700

    Fixing Images

--- app/ui/info-output.tsx
+++ app/ui/info-output.tsx
@@ -75,7 +75,7 @@ export async function InfoBox(input: string){
         count++;
     }
 
-    const path="/desert.jpg";
+    const path="/Desert.jpg";
     return(
         <>  
             <div className="drop-shadow-xl min-w-[85vw] max-w-[85vw] w-fit text-wrap justify:center text-center item-center p-1 bg-[#42414d] rounded">
