5e6babe1ea849d0170d088a36c2892d175be9798
Fraser Kent <fwpkent@gmail.com>
2026-05-01 21:38:45 -0700

    Fix

--- app/ui/shared/header_icon.tsx
+++ app/ui/shared/header_icon.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import LogInButton from "../homepage/login-button";
+import LogInButton from "./login-button";
 export default function HeaderIcon(){
     return(
         <>
